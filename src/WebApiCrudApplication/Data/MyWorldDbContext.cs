083dfd4e37f1a9066433376199421f6d64e917f8
michu3136 <71141129+michu3136@users.noreply.github.com>
2022-02-08 17:35:46 +0100

    Add Cake -Model to entities.

---
+++ src/WebApiCrudApplication/Data/MyWorldDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiCrudApplication.Data.Entities;
+
+namespace WebApiCrudApplication.Data
+{
+    public class MyWorldDbContext:DbContext
+    {
+        public MyWorldDbContext(DbContextOptions<MyWorldDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Cake> Cake { get; set; }
+
+    }
+}
